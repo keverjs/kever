87fe9c4127118cbe463934ef0f06f70b5e301537
hubvue <1191340528@qq.com>
2019-12-01 12:21:28 +0800

    dev: eslint

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,24 +1,19 @@
-import {instancePoll} from '../ioc'
-import {
-  META_INJECT,
-  META_PROVIDE,
-  META_CONTROLLER
-} from '../constants'
+import { instancePoll } from '../ioc'
+import { META_INJECT, META_PROVIDE, META_CONTROLLER } from '../constants'
 import { RuntimeOptions } from '../interface'
 import KoaRuntime from '../runtime/koa'
-
+import * as Koa from 'koa'
 export const createApplication = (options: RuntimeOptions = {}) => {
-  const controllers: Set<any> = new Set();
+  const controllers: Set<any> = new Set()
   const controllerPoll: Map<any, any> = instancePoll.getAll(META_INJECT)
-  for(let [Controller, tag] of controllerPoll) {
-    if(instancePoll.has(META_PROVIDE,tag)) {
-      const injectableModel: (new () => {}) = instancePoll.get(META_PROVIDE,tag)
+  for (let [Controller, tag] of controllerPoll) {
+    if (instancePoll.has(META_PROVIDE, tag)) {
+      const injectableModel: new () => {} = instancePoll.get(META_PROVIDE, tag)
       const controller = new Controller(new injectableModel())
       controllers.add(controller)
     } else {
       throw new Error(`not ${tag} model is injectable`)
     }
-    
   }
   const app = KoaRuntime(controllers, options)
   return app
