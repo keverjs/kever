f8cd42566e10902689d0e0316a05c3eb6b6519b4
hubvue <1191340528@qq.com>
2019-12-10 22:34:32 +0800

    docs: package.json add keywords

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,16 +1,21 @@
 /**
- *
+ * @description 各可反射的常量标识
  */
 export const META_ROUTER = Symbol.for('dectorator#meta_router')
-/**
- *
- */
-export const META_HTTP_METHOD = Symbol.for('dectorator#meta_http_method')
-/**
- *
- */
+
 export const META_CONTROLLER = Symbol.for('dectorator#meta_controller')
-/**
- *
- */
+
 export const META_INJECT = Symbol.for('dectorator#meta_inject')
+
+export const META_HEADER = Symbol.for('decorator#meta_header')
+
+export const META_PARAMS = Symbol.for('decorator#meta_params')
+
+export const META_BODY = Symbol.for('decorator#meta_body')
+
+export const META_COOKIE = Symbol.for('decorator#meta_cookie')
+
+// 未来要做的
+export const META_PASSPORT = Symbol.for('decorator#meta_passport')
+export const META_RPC_CLIENT = Symbol.for('decorator#meta_rpc_client')
+export const META_RPC_SERVER = Symbol.for('decorator#meta_rpc_server')
