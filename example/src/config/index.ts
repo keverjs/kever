ba6660386b34f25689120589c332ebd622ac5d51
hubvue <1191340528@qq.com>
2019-12-21 15:30:49 +0800

    refactor: 重写example

---
+++ example/src/config/index.ts
@@ -0,0 +1,8 @@
+export default () => {
+  const config = {
+    port: 9000,
+    host: '127.0.0.1',
+    plugins: []
+  }
+  return config
+}
--- example/sunnier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [],
-  port: 9000,
-  loadPath: {
-    controller: './dist/controllers',
-    service: './dist/service'
-  }
-}
