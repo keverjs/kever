6d830058cd701021434a2b25e6b37dd4b1653ff5
hubvue <1191340528@qq.com>
2021-04-25 19:55:48 +0800

    feat: thread

--- packages/logger/src/utils/index.ts
+++ packages/logger/src/utils/index.ts
@@ -1,3 +1,18 @@
+import * as fs from 'fs'
+const { stat, mkdir } = fs.promises
+
 export const _processDate = (date: number): string | number => {
   return date < 10 ? `0${date}` : date
+}
+
+/**
+ * @description 检查log目录是否存在，不存在则创建
+ * @param logDir
+ */
+export const _checkFolderExistence = async (logDir: string) => {
+  try {
+    await stat(logDir)
+  } catch (err) {
+    await mkdir(logDir)
+  }
 }
