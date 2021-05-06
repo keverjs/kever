a39cb02e2887dd68f67b1da6d417c66311d6eb73
hubvue <1191340528@qq.com>
2021-05-06 16:02:27 +0800

    feat: model add toJson init methods

--- packages/ioc/src/model/index.ts
+++ packages/ioc/src/model/index.ts
@@ -11,7 +11,14 @@ type Setter<T extends object> = {
 }
 
 
-type GS<T extends object> = Getter<T> & Setter<T>
+type GSAccessor<T extends object> = Getter<T> & Setter<T>
+
+interface ModelInstanceMethods<T extends object> {
+  init<V extends T>(value: V | string): void
+  toJson(): string
+}
+
+type ModelInstance<T extends object> = GSAccessor<T> & ModelInstanceMethods<T>
 
 const modelPool = new InstancePool<Tag, Function>()
 
@@ -21,27 +28,43 @@ export const Model = (tag: Tag): ClassDecorator => {
   }
 }
 
-Model.use = <T extends object>(tag: Tag): GS<T> => {
+Model.use = <T extends object>(tag: Tag): ModelInstance<T> => {
   const modelConstructor = modelPool.use(tag)
   if (!modelConstructor) {
     logger.error(`${tag.toString()} type model no exists`)
-    return {} as GS<T>
+    return {} as ModelInstance<T>
   }
   const instance = new (modelConstructor as InstanceType)()
   const proxy = new Proxy(instance, {
     get(target, property: string, receiver) {
-      const prefix = property.slice(0, 3)
-      const key = property
-        .slice(3)
-        .replace(/([A-Z])/, (match) => match.toLowerCase())
+      let prefix: string, key: string
+      if (property === 'init' || property === 'toJson') {
+        prefix = property
+      } else {
+        prefix = property.slice(0, 3)
+        key = property
+          .slice(3)
+          .replace(/([A-Z])/, (match) => match.toLowerCase())
+      } 
 
       return (value: unknown) => {
-        console.log('taget', target)
         if (prefix === 'get') {
           return Reflect.get(target, key, receiver)
         }
         if (prefix === 'set') {
           Reflect.set(target, key, value, receiver)
+        }
+        if (prefix === 'toJson') {
+          return JSON.stringify(target)
+        }
+        if (prefix === 'init') {
+          let object = value as Record<string, unknown>
+          if (typeof value === 'string') {
+            object = JSON.parse(value)
+          }
+          Object.keys(object).forEach(key => {
+            Reflect.set(target, key, object[key], receiver)
+          })
         }
       }
     },
