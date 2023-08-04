68ceb35600561055c4c4c45e32221b50e0d353af
kimwangchong <kimwangchong@didiglobal.com>
2023-08-04 21:11:13 +0800

    feat: add context enhance: setHeader getHeader

--- packages/router/src/enhance/body.ts
+++ packages/router/src/enhance/body.ts
@@ -1,12 +1,19 @@
 import { type Context, type KoaContext } from '@kever/shared'
 
+interface ConsideredRequest {
+  method: string
+  body: Record<string, unknown> | undefined
+}
+
 export const enhanceBody = (ctx: KoaContext): Context => {
   ctx.getBody = <T>(key?: string): T | undefined => {
-    if (ctx.request.method === 'post' && ctx.body) {
+    ctx.header
+    const { method, body } = ctx.request as unknown as ConsideredRequest
+    if (method === 'post' && body) {
       if (key) {
-        return (ctx.body as Record<string, unknown>)[key] as T
+        return body[key] as T
       }
-      return ctx.body as T
+      return body as T
     }
     return undefined
   }
--- packages/router/src/enhance/declare.ts
+++ packages/router/src/enhance/declare.ts
@@ -1,10 +1,11 @@
 import type { Logger } from '@kever/core'
 
-
 declare module '@kever/shared' {
   export interface Context {
     logger: Logger
     getBody: <T>(key?: string) => T | undefined
     getQuery: <T>(key?: string) => T | undefined
+    getHeader: (key: string) => string | undefined
+    setHeader: (key: string, value: string) => void
   }
 }
