cadb2592f6e0c4e242f02368f264078bf36182aa
kimwangchong <kimwangchong@didiglobal.com>
2023-08-09 11:58:55 +0800

    fix(enhance): body exist

--- packages/enhance/src/context/handers.ts
+++ packages/enhance/src/context/handers.ts
@@ -12,9 +12,8 @@ interface ConsideredRequest {
  */
 export const enhanceBody = (ctx: KoaContext): Context => {
   ctx.getBody = <T>(key?: string): T | undefined => {
-    ctx.header
     const { method, body } = ctx.request as unknown as ConsideredRequest
-    if (method === 'post' && body) {
+    if (method === 'POST' && body) {
       if (key) {
         return body[key] as T
       }
